38ad26d841459dce407e09962f5094532d68148f
sead <sead@zuehlke.com>
2015-06-16 18:28:30 +0200

    MvvmCross forms droid does not work!

---
+++ GroceryShopper/GroceryShopper.Forms/GroceryShopper.Forms.Droid/Setup.cs
@@ -0,0 +1,33 @@
+using Android.Content;
+using Cheesebaron.MvxPlugins.FormsPresenters.Core;
+using Cheesebaron.MvxPlugins.FormsPresenters.Droid;
+using Cirrious.CrossCore;
+using Cirrious.MvvmCross.Droid.Platform;
+using Cirrious.MvvmCross.Droid.Views;
+using Cirrious.MvvmCross.ViewModels;
+using Cirrious.MvvmCross.Views;
+using GroceryShopper.Core;
+
+namespace GroceryShopper.Forms.Droid
+{
+    class Setup : MvxAndroidSetup
+    {
+        public Setup(Context applicationContext)
+            : base(applicationContext)
+        {
+        }
+
+        protected override IMvxApplication CreateApp()
+        {
+            return new App();
+        }
+
+        protected override IMvxAndroidViewPresenter CreateViewPresenter()
+        {
+            var presenter = new MvxFormsDroidPagePresenter();
+            Mvx.RegisterSingleton<IMvxViewPresenter>(presenter);
+
+            return presenter;
+        }
+    }
+}
