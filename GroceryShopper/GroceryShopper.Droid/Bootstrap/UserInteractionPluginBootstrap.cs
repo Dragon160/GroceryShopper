02ecb5bf8d6dc95c4a7ca62dfd8c2e073df09108
mip <mip@zuehlk.com>
2015-06-17 14:16:12 +0200

    deleted old projects

--- GroceryShopper/GroceryShopper.Droid/Bootstrap/UserInteractionPluginBootstrap.cs
+++
@@ -1,9 +0,0 @@
-using Cirrious.CrossCore.Plugins;
-
-namespace GroceryShopper.Droid.Bootstrap
-{
-    public class UserInteractionPluginBootstrap
-        : MvxPluginBootstrapAction<Chance.MvvmCross.Plugins.UserInteraction.PluginLoader>
-    {
-    }
-}
