2994e5018f32334fa7fbc03100e51c1b97ced048
sead <sead@zuehlke.com>
2015-06-17 09:59:19 +0200

    added unit test projekct

---
+++ GroceryShopper/GroceryShopper.Forms/GroceryShopper.Forms/Services/FakedService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using GroceryShopper.Forms.Interfaces;
+
+namespace GroceryShopper.Forms.Services
+{
+    public class FakedService : IFakedService
+    {
+        public async Task<int> DoSomeMagicStuffAsync(int seed)
+        {
+            var rand = new Random(seed).Next();
+            await Task.Delay(100);
+            return rand;
+
+        }
+    }
+}
